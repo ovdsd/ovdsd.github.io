bdd9555506b861947f4e1a736afcdac3497cb023
ovdsd <81808543+LeonClausius@users.noreply.github.com>
2024-06-25 00:19:45 +0700

    .

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -29,12 +29,12 @@ const Clients = () => {
           {/* {companies.map((company) => (
             <React.Fragment key={company.id}>
               <div className="flex md:max-w-60 max-w-32 gap-2">
-                <img
+                <Image
                   src={company.img}
                   alt={company.name}
                   className="md:w-10 w-5"
                 />
-                <img
+                <Image
                   src={company.nameImg}
                   alt={company.name}
                   width={company.id === 4 || company.id === 5 ? 100 : 150}
--- components/ui/InfiniteCards.tsx
+++ components/ui/InfiniteCards.tsx
@@ -2,6 +2,7 @@
 
 import { cn } from "@/lib/utils";
 import React, { useEffect, useState } from "react";
+import Image from "next/image";
 
 export const InfiniteMovingCards = ({
   items,
@@ -117,7 +118,7 @@ export const InfiniteMovingCards = ({
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 {/* add this div for the profile img */}
                 <div className="me-3">
-                  <img src={item.img} alt="profile" height="50" width="50" />
+                  <Image src={item.img} alt="profile" height={50} width={50} />
                 </div>
                 <span className="flex flex-col gap-1">
                   {/* change text color, font-normal to font-bold, text-xl */}
