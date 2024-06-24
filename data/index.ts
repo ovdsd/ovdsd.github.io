5393dfe4aa4e05936ba9a9d7adeb298abb83513b
ovdsd <81808543+LeonClausius@users.noreply.github.com>
2024-06-24 22:20:57 +0700

    new one

---
+++ data/index.ts
@@ -0,0 +1,215 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Prioritize client wishes, offering open communication",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Developing with Creativity.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Weather API",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Interested in starting a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "GoFruit.id",
+    des: "Manage your product inventory using our inventory system. Seamlessly Inventory system using GoFruit.id.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/mysql.svg", "/python.svg"],
+    link: "/GoFruit.id",
+    plink: "GoFruit.id",
+  },
+  {
+    id: 2,
+    title: "Mekitam - Video Conferencing Website",
+    des: "Lift up your video conference experience using mekitam.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/js.svg", "/php.svg"],
+    link: "/mekitam.com",
+    plink: "mekitam.com",
+  },
+  // {
+  //   id: 3,
+  //   title: "AI Image SaaS - Canva Application",
+  //   des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+  //   img: "/p3.svg",
+  //   iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+  //   link: "/ui.aiimg.com",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Animated Apple Iphone 3D Website",
+  //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+  //   img: "/p4.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+  //   link: "/ui.apple.com",
+  // },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "I had the pleasure of working with Is'ad on our latest game project, and the experience was nothing short of exceptional. From the initial concept phase to the final release, Is'ad demonstrated an impressive level of skill, creativity, and dedication. If you're seeking game developer with infinity ideas, Is'ad is the ideal partner.",
+    name: "Atta",
+    title: "Owner of Dot Roleplay",
+    img: "/profile.svg",
+  },
+  {
+    quote:
+      "Collaborating with Is'ad was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Is'ad's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website API, Is'ad is the ideal partner.",
+    name: "Kent Bryant",
+    title: "Head Developer of GoFruit.id",
+    img: "https://cdn.discordapp.com/attachments/961820020302815292/1254809405967437946/image.png?ex=667ad7d3&is=66798653&hm=032c3576c57ddcb8fe7f6db606889a97c3c1f0ebba1830efef9042e160834e6e&",
+  },
+  {
+    quote:
+      "Best Game Developer. His professionalism and dedication to our game was epic and the results were amazing. If you're searching game developer for your game, Is'ad gonna be your best choice.",
+    name: "Ali Assegaf",
+    title: "Director of Garuda Indonesia Roblox",
+    img: "https://cdn.discordapp.com/attachments/961820020302815292/1254810500362145833/image.png?ex=667ad8d8&is=66798758&hm=8bc887e8669ec51b74484be7c29fb077e2a0a28016453da6b3be6f190dcef753&",
+  },
+  // {
+  //   quote:
+  //     "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+  //   name: "Michael Johnson",
+  //   title: "Director of AlphaStream Technologies",
+  // },
+  // {
+  //   quote:
+  //     "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+  //   name: "Michael Johnson",
+  //   title: "Director of AlphaStream Technologies",
+  // },
+];
+
+export const companies = [
+  // {
+  //   id: 1,
+  //   name: "cloudinary",
+  //   img: "/cloud.svg",
+  //   nameImg: "/cloudName.svg",
+  // },
+  // {
+  //   id: 2,
+  //   name: "appwrite",
+  //   img: "/app.svg",
+  //   nameImg: "/appName.svg",
+  // },
+  // {
+  //   id: 3,
+  //   name: "HOSTINGER",
+  //   img: "/host.svg",
+  //   nameImg: "/hostName.svg",
+  // },
+  // {
+  //   id: 4,
+  //   name: "stream",
+  //   img: "/s.svg",
+  //   nameImg: "/streamName.svg",
+  // },
+  // {
+  //   id: 5,
+  //   name: "docker.",
+  //   img: "/dock.svg",
+  //   nameImg: "/dockerName.svg",
+  // },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Game Developer",
+    desc: "Game developer using Lua, Developed roblox and fivem games",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Backend Developer",
+    desc: "Developed and maintained API using Djanggo RestAPI and backing it up using Datadog.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp4.svg",
+  },
+  // {
+  //   id: 3,
+  //   title: "Lead Frontend Developer",
+  //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+  //   className: "md:col-span-2",
+  //   thumbnail: "/exp4.svg",
+  // },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
